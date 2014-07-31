c0cc207e9f293734c60dd89d1fd5b69dfa561b45
Maksim Kis <maksimkis@79-104-202-51.ranetka.ru>
2014-08-01 01:36:32 +0800

    Facebook start

---
+++ cribysutils/ios/Social/Facebook/FacebookController.h
@@ -0,0 +1,17 @@
+//
+//  FacebookController.h
+//  cocos2d_libs
+//
+//  Created by Maksim Kis on 01.08.14.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FacebookController : NSObject
+{
+    
+}
+
+-(bool)isSessionActive();
+@end
---
+++ cribysutils/ios/Social/Facebook/FacebookController.m
@@ -0,0 +1,28 @@
+//
+//  FacebookController.m
+//  cocos2d_libs
+//
+//  Created by Maksim Kis on 01.08.14.
+//
+//
+
+#import "FacebookController.h"
+#import <FacebookSDK/FacebookSDK.h>
+
+@implementation FacebookController
+
+
+-(bool) isSessionActive() {
+    bool return_value = NO;
+    
+    if(FBSession.activeSession.state == FBSessionStateOpen) {
+        return_value =  YES;
+    }
+    return return_value;
+}
+
+-(bool) login() {
+    
+}
+
+@end
